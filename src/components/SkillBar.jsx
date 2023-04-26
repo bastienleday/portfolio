18b5ff3775dfe096d0362ac779f0580a57bd5ea4
bastienleday <bastien.leday@gmail.com>
2023-04-26 18:31:15 +0200

    card

--- src/components/SkillBar.jsx
+++ src/components/SkillBar.jsx
@@ -2,16 +2,11 @@ import react, { useEffect, useRef, useState } from "react";
 import Skills from "./Skills.jsx";
 
 export default function SkillBar({ children }) {
-
-
 	const skillContainer = useRef(null);
 
-
-
 	useEffect(() => {
-
 		const options = {
-			threshold: 0.5, // Le pourcentage de visibilité de l'élément dans le viewport pour déclencher l'animation
+			threshold: 1, // Le pourcentage de visibilité de l'élément dans le viewport pour déclencher l'animation
 		};
 
 		const skillBarMove = (entries, observer) => {
@@ -19,13 +14,13 @@ export default function SkillBar({ children }) {
 				if (entry.isIntersecting) {
 					// Vérifier si l'élément est visible dans le viewport
 					gsap.to(entry.target, {
-						y: 250,
-						duration: 2,
+						y: 280,
+						duration: 1,
 						delay: 0.2,
 						scrollTrigger: {
 							trigger: ".skillContainer",
-
-						}
+							start: "top 20%",
+						},
 					});
 					observer.unobserve(entry.target); // Arrêter d'observer une fois que l'animation est déclenchée
 				}
@@ -41,14 +36,11 @@ export default function SkillBar({ children }) {
 			// Nettoyer l'observer lors du démontage du composant
 			observer.disconnect();
 		};
-
-
 	}, []);
 
 	return (
 		<div className="skillContainer">
 			<div className="skillContent" ref={skillContainer}>
-
 				{children}
 			</div>
 		</div>
