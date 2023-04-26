2f5652d380a2756bf0b08e2305cf57e9f8acab24
bastienleday <bastien.leday@gmail.com>
2023-04-26 15:15:40 +0200

    gsap

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,80 +1,69 @@
-import react, {useEffect} from 'react';
-import './componentsCSS/Home.css';
+import react, { useEffect } from "react";
+import "./componentsCSS/Home.css";
 import Welcome from "./Welcome.jsx";
 import NavBar from "./NavBar.jsx";
 import Next from "./Next.jsx";
 import Viewer from "./Viewer.jsx";
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from "@react-three/fiber";
 import responsiveCSS from "./componentsCSS/responsiveCSS.css";
 import Content from "./componentsCSS/Content.css";
 import Skills from "./Skills.jsx";
-import {useRef} from "react";
+import { useRef } from "react";
 import SkillBar from "./SkillBar.jsx";
-
-
-
-
-
-
+import ProjectCard from "./ProjectCard";
 
 export default function Home() {
-
-
-
-
-    return (
-        <>
-
-        <div className="home">
-            <NavBar/>
-            <div className="gros">
-
-            <div className="homeContent">
-
-
-                 <Welcome/>
-                         <h1 className="title">
-                            Bastien Leday, <br/> Front End Developer
-                        </h1>
-                        <p className="presentation">
-                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rhoncus pharetra ex laoreet cursus. Maecenas vitae orci et dui faucibus molestie. Sed ipsum magna, dignissim ut enim sit amet, hendrerit porttitor enim. Nunc sed ultrices magna, sed sagittis dui. Integer tincidunt finibus enim imperdiet accumsan. Sed aliquet aliquet leo, a lacinia est dignissim in. Aliquam sollicitudin viverra enim a dapibus. Aliquam elit ante, vulputate a sollicitudin eu, mattis non nunc.
-                        </p>
-            </div>
-                <div className="canvasContainer">
-                    <Canvas>
-                <Viewer/>
-                    </Canvas>
-                </div>
-            </div>
-            <div className="nextContainer">
-                <Next/>
-
-            </div>
-        </div>
-          <SkillBar>
-              <Skills logo={"/public/skillsLogo/nodejs.png"}/>
-              <Skills logo={"/public/skillsLogo/ps.png"}/>
-              <Skills logo={"/public/skillsLogo/psql.png"}/>
-              <Skills logo={"/public/skillsLogo/react.png"}/>
-              <Skills logo={"/public/skillsLogo/substance.png"}/>
-              <Skills logo={"/public/skillsLogo/threejs.png"}/>
-              <Skills logo={"/public/skillsLogo/blender.png"}/>
-              <Skills logo={"/public/skillsLogo/js.png"}/>
-              <Skills logo={"/public/skillsLogo/nodejs.png"}/>
-              <Skills logo={"/public/skillsLogo/ps.png"}/>
-              <Skills logo={"/public/skillsLogo/psql.png"}/>
-              <Skills logo={"/public/skillsLogo/react.png"}/>
-              <Skills logo={"/public/skillsLogo/substance.png"}/>
-              <Skills logo={"/public/skillsLogo/threejs.png"}/>
-            </SkillBar>
-            <div className="projectsContainer">
-
-            </div>
-        </>
-
-
-
-
-
-    )
+	return (
+		<>
+			<div className="home">
+				<NavBar />
+				<div className="gros">
+					<div className="homeContent">
+						<Welcome />
+						<h1 className="title">
+							Bastien Leday, <br /> Front End Developer
+						</h1>
+						<p className="presentation">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+							rhoncus pharetra ex laoreet cursus. Maecenas vitae orci et dui
+							faucibus molestie. Sed ipsum magna, dignissim ut enim sit amet,
+							hendrerit porttitor enim. Nunc sed ultrices magna, sed sagittis
+							dui. Integer tincidunt finibus enim imperdiet accumsan. Sed
+							aliquet aliquet leo, a lacinia est dignissim in. Aliquam
+							sollicitudin viverra enim a dapibus. Aliquam elit ante, vulputate
+							a sollicitudin eu, mattis non nunc.
+						</p>
+					</div>
+					<div className="canvasContainer">
+						<Canvas>
+							<Viewer />
+						</Canvas>
+					</div>
+				</div>
+				<div className="nextContainer">
+					<Next />
+				</div>
+			</div>
+			<SkillBar>
+				<Skills logo={"/public/skillsLogo/nodejs.png"} />
+				<Skills logo={"/public/skillsLogo/ps.png"} />
+				<Skills logo={"/public/skillsLogo/psql.png"} />
+				<Skills logo={"/public/skillsLogo/react.png"} />
+				<Skills logo={"/public/skillsLogo/substance.png"} />
+				<Skills logo={"/public/skillsLogo/threejs.png"} />
+				<Skills logo={"/public/skillsLogo/blender.png"} />
+				<Skills logo={"/public/skillsLogo/js.png"} />
+				<Skills logo={"/public/skillsLogo/nodejs.png"} />
+				<Skills logo={"/public/skillsLogo/ps.png"} />
+				<Skills logo={"/public/skillsLogo/psql.png"} />
+				<Skills logo={"/public/skillsLogo/react.png"} />
+				<Skills logo={"/public/skillsLogo/substance.png"} />
+				<Skills logo={"/public/skillsLogo/threejs.png"} />
+			</SkillBar>
+			<div className="projectsContainer">
+				<ProjectCard />
+				<ProjectCard />
+			</div>
+		</>
+	);
 }
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function ProjectCard({ title, description, image, link }) {
+	const main = useRef(null);
+	useEffect(() => {
+		const options = {
+			threshold: 0.2, // Le pourcentage de visibilité de l'élément dans le viewport pour déclencher l'animation
+		};
+
+		const callback = (entries, observer) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					// Vérifier si l'élément est visible dans le viewport
+					gsap.to(entry.target, {
+						x: 200,
+						duration: 2,
+					});
+					observer.unobserve(entry.target); // Arrêter d'observer une fois que l'animation est déclenchée
+				}
+			});
+		};
+
+		const observer = new IntersectionObserver(callback, options);
+		if (main.current) {
+			observer.observe(main.current); // Commencer à observer l'élément au chargement du composant
+		}
+
+		return () => {
+			// Nettoyer l'observer lors du démontage du composant
+			observer.disconnect();
+		};
+	}, []);
+
+	return (
+		<div className="projectCard" id="projectCard" ref={main}>
+			<h1>carte 1</h1>
+		</div>
+	);
+}
