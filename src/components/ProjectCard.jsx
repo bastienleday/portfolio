1f533e19a35f32738b703e96334d6a4108554982
bastienleday <bastien.leday@gmail.com>
2023-04-28 18:57:25 +0200

    animation responsive

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useMediaQuery} from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 
 // Register ScrollTrigger with gsap
 gsap.registerPlugin(ScrollTrigger);
 
-
-export default function ProjectCard({children, techno }) {
+export default function ProjectCard({ children, techno }) {
 	const isMobile = useMediaQuery("(max-width: 1000px)");
-	const isLaptop = useMediaQuery("(max-width: 1800px)");
-
+	const isLaptop = useMediaQuery("(max-width: 1950px)");
 
 	const main = useRef(null);
 	useEffect(() => {
@@ -24,20 +22,22 @@ export default function ProjectCard({children, techno }) {
 				if (entry.isIntersecting) {
 					// Vérifier si l'élément est visible dans le viewport
 					gsap.to(entry.target, {
-						x: isMobile ? 0 : isLaptop ? 0 : 900,
+						opacity: 1,
+						x: isMobile ? 0 : isLaptop ? 600 : 900,
 						duration: 1,
 						delay: 0.2,
 						scrollTrigger: {
 							trigger: ".skillContainer",
 							start: "top 80%",
-							end:  "bottom 20%",
+							end: "bottom 20%",
+
 							toggleActions: "restart none none reverse",
-
-
-
-
-						}
-
+							markers: {
+								startColor: "red",
+								endColor: "red",
+								fontSize: "20px",
+							},
+						},
 					});
 					observer.unobserve(entry.target); // Arrêter d'observer une fois que l'animation est déclenchée
 				}
@@ -62,9 +62,7 @@ export default function ProjectCard({children, techno }) {
 				{techno.map((techno, index) => (
 					<li key={index}>{techno}</li>
 				))}
-
 			</ul>
-
 		</div>
 	);
 }
