dc62de9d370d6a3430f54c179825a09d9513e9fb
unknown <you@example.com>
2023-04-28 09:03:06 +0200

    color setting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
 
     return (
         <div className="footer">
-            <p className="footerText">© 2021 - All rights reserved</p>
+            <p className="footerText">© 2023 - All rights reserved</p>
         </div>
     )
 }
