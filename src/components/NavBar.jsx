ccd113514519aef89291706491077da4597460f0
bastienleday <bastien.leday@gmail.com>
2023-04-28 19:01:30 +0200

    link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,23 @@
-
 import CircularLink from "./CircularLink.jsx";
 
 export default function NavBar() {
-
-    return (
-        <nav className="navBar">
-
-            <div className="circularLinkContainer">
-                <CircularLink link={'https://www.linkedin.com/in/bastien-leday/'} >
-                    <img src={'/pictures/lin.png'} alt={""} className="gitLogo"></img>
-                </CircularLink>
-                <CircularLink link={'https://github.com/bastienleday'}>
-                    <img src={'/pictures/git.png'} alt={""} className="gitLogo"></img>
-                </CircularLink>
-                <CircularLink link={'https://www.instagram.com/'}>
-                    <img src={'/pictures/instagram.png'} className="instagramLogo" alt={""}>
-                    </img>
-                </CircularLink>
-
-            </div>
-
-
-
-
-
-
-
-        </nav>
-    )
+	return (
+		<nav className="navBar">
+			<div className="circularLinkContainer">
+				<CircularLink link={"https://www.linkedin.com/in/bastien-leday/"}>
+					<img src={"/pictures/lin.png"} alt={""} className="gitLogo"></img>
+				</CircularLink>
+				<CircularLink link={"https://github.com/bastienleday"}>
+					<img src={"/pictures/git.png"} alt={""} className="gitLogo"></img>
+				</CircularLink>
+				<CircularLink link={"https://www.instagram/bastienleday.com/"}>
+					<img
+						src={"/pictures/instagram.png"}
+						className="instagramLogo"
+						alt={""}
+					></img>
+				</CircularLink>
+			</div>
+		</nav>
+	);
 }
